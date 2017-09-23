595c650e753a8aecb3ab01810c38c7190517afa3
sammillos <millossamantha@gmail.com>
2017-09-23 16:44:12 +0800

    Binura ko lang yung isang link dyan

--- application/views/include/header.php
+++ application/views/include/header.php
@@ -7,7 +7,6 @@
     <title>MimO</title>
     <link rel="stylesheet" href="http://localhost/mimo/assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="http://localhost/mimo/assets/fonts/material-icons.css">
-    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
     <link rel="stylesheet" href="http://localhost/mimo/assets/css/custom.css">
     <link rel="stylesheet" href="http://localhost/mimo/assets/css/photoupload.css">
 	<script type="text/javascript" src="http://localhost/mimo/assets/js/jquery.min.js"></script>
