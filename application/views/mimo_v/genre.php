513cea2c5e8b4cebcea5c80b2c2408924f895bf1
sammillos <millossamantha@gmail.com>
2017-10-08 23:45:32 +0800

    Update genre.php

--- application/views/mimo_v/genre.php
+++ application/views/mimo_v/genre.php
@@ -1,54 +1,6 @@
 <body style="background-color: #333333">
 	<!-- Top Nav Bar -->
-<<<<<<< HEAD
 	<?php $this->load->view('include/topnav');?>
-=======
-  <nav class="navbar navbar-inverse navbar-fixed-top">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <a class="navbar-brand navbar-link" href="<?php echo base_url('');?>">
-				<?php $this->load->view('include/mimologo')?>
-				<strong style="margin-top: -5px;">MimO</strong> </a>
-                <button class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button>
-            </div>
-            <div class="collapse navbar-collapse" id="navcol-1">
-			 <form class="navbar-form navbar-left">
-                    <div class="input-group">
-						<!--Search Bar -->
-                        <input class="form-control searchbar" type="text" placeholder="Search">
-                        <div class="input-group-btn">
-                            <a class="btn btn-block searchBtn" role="button" href="#"> <i class="glyphicon glyphicon-search"></i></a>
-                        </div>
-                    </div>
-                </form>
-				<!-- Nav Items -->
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="presentation" role="presentation"><a href="<?php echo base_url('');?>" class="bar">
-						<i class="material-icons bar">speaker</i><p class="bar hidden-sm"> Music Hall</p></a></li>
-                    <li class="presentation" role="presentation"><a href="<?php echo base_url('mimo/browse');?>" class="bar">
-						<i class="material-icons bar">equalizer</i><p class="hidden-sm bar"> Browse</p></a>
-                    </li>
-					 <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false" href="#" class="bar">
-						<i class="material-icons bar">file_upload</i><p class="hidden-sm bar"> Release</p><span class="caret"></span></a>
-						 <ul class="dropdown-menu" role="menu">
-                            <li role="presentation"><a href="" data-toggle="modal" data-target="#addThought"><i class="material-icons">comments</i> Thoughts</a></li>
-                            <li role="presentation"><a href="" data-toggle="modal" data-target="#addAudio"><i class="material-icons">music_note</i> Audio</a></li>
-						</ul>
-                    <li role="presentation"><a href="<?php echo base_url('mimo/myStudio');?>" class="bar">
-						<i class="material-icons bar">headset_mic</i><p class="hidden-sm bar">My Studio</p></a></li>	
-					 <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false" href="#" class="bar">
-						<i class="material-icons">settings</i> <span class="caret"></span></a>
-						 <ul class="dropdown-menu" role="menu">
-                            <li role="presentation">
-								<a href="<?php echo base_url('mimo/settings');?>">Account Settings</a></li>
-                            <li role="presentation"><a href="#">Logout</a></li>
-						</ul>
-                </ul>
-            </div>
-        </div>
-    </nav>
-	
->>>>>>> master
 	<!--This is below Top Navigation Section -->
 	
 <div class="container " style="padding-top:50px;">
@@ -56,27 +8,6 @@
 	<h3 class="txt" style="color:white;">Genres</h3>
 </div>
 
-<<<<<<< HEAD
-
-
-
-	</body>
-		</div>
-		
-			<div class="col-md-1">
-			
-			</div>
-			
-			
-	</div>
-		<br/>
-		<br/>
-		<br/><br/>
-		<br/>
-		<br/>
-		<?php $this->load->view('include/releasebuttons');?>
-</div>		
-=======
 <div class="row text-center" style="padding-top:50px;">
 	<div class="col-md-2 " >
 		<div >
@@ -228,5 +159,5 @@
 	</div>
 	</div>
 	<!--end of first row-->	
->>>>>>> master
+	<?php $this->load->view('include/releasebuttons');?>
 </body>
