ec83ff6cd8e3707f4f4e26618d003db11254ff88
Izzay <izzaybene@gmail.com>
2017-10-14 21:32:43 +0800

    yung delete modal

--- application/views/templates/delete_modal.php
+++ application/views/templates/delete_modal.php
@@ -1,6 +1,6 @@
 <!-- Trigger the modal with a button -->
 <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Delete</button>
-
+ 
 <!-- Modal -->
 <div id="myModal" class="modal fade" role="dialog">
   <div class="modal-dialog">
