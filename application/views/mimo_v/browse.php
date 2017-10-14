3249efe0a674380b5cc52bc9be7202535d1482cc
Izzay <izzaybene@gmail.com>
2017-10-14 23:17:09 +0800

    browse po

--- application/views/mimo_v/browse.php
+++ application/views/mimo_v/browse.php
@@ -49,7 +49,7 @@
 			<div><a href="<?php echo base_url('mimo/playlist');?>"><img src="http://localhost/mimo/assets/img/vir_gl.jpg" class="img-rounded img-chart " style="width:200px; height; 200px;"></a></div>				
 			<div><strong><a href="<?php echo base_url('mimo/playlist');?>" class="txt" style="color:black";>Global Viral 50</a></strong></div>
 		</div>
-	</div>
+	</div> 
   </div>
   
 
@@ -58,8 +58,8 @@
 	<div class="col-md-3 " >
 		<div >
 			<!--both a href link papunta sa playlist-->
-			<div><a href="/Mimo/playlist" ><img src="http://localhost/mimo/assets/img/alt.png" class="img-rounded img-chart "></a></div>			
-			<div><a href="/Mimo/playlist" class="txt ">Alternative Music</a></div>	
+			<div><a href="/Mimo/playlist" ><img src="http://localhost/mimo/assets/img/alt.png" class="img-rounded img-chart" ></a></div>			
+			<div><a href="/Mimo/playlist" class="txt " style="color:black">Alternative Music</a></div>	
 		</div>
 	</div>
 	
@@ -68,7 +68,7 @@
 		<div>
 		<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/blues.jpg" class="img-rounded img-chart " style="width:170px;height:170px;"></a></div>				
-			<div><a href="/Mimo/playlist" class="txt ">Blues</a></div>	
+			<div><a href="/Mimo/playlist" class="txt " style="color:black">Blues</a></div>	
 		</div>
 	</div>
 	
@@ -76,7 +76,7 @@
 		<div>
 			<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/classical.png" class="img-rounded img-chart " style="width:170px;height:170px"></a></div>					
-			<div><a href="/Mimo/playlist" class="txt">Classical Music</a></div>	
+			<div><a href="/Mimo/playlist" class="txt" style="color:black">Classical Music</a></div>	
 		</div>
 	</div>
 	
@@ -84,14 +84,14 @@
 		<div>
 			<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/country.png" class="img-rounded img-chart"  style="width:170px;height:170px "></a></div>				
-			<div><a href="/Mimo/playlist" class="txt">Country Music</a></div>
+			<div><a href="/Mimo/playlist" class="txt" style="color:black">Country Music</a></div>
 		</div>
 	</div>
 	<div class="col-md-3 " >
 		<div>
 		<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/dance.jpg" class="img-rounded img-chart " style="width:170px;height:170px;"></a></div>				
-			<div><a href="/Mimo/playlist" class="txt ">Dance Music</a></div>	
+			<div><a href="/Mimo/playlist" class="txt" style="color:black">Dance Music</a></div>	
 		</div>
 	</div>
 	
@@ -99,7 +99,7 @@
 		<div>
 			<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/easy.png" class="img-rounded img-chart " style="width:170px;height:170px"></a></div>					
-			<div><a href="/Mimo/playlist" class="txt">Easy Listening</a></div>	
+			<div><a href="/Mimo/playlist" class="txt" style="color:black">Easy Listening</a></div>	
 		</div>
 	</div>
 	
@@ -108,7 +108,7 @@
 		<div>
 		<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/electronic.jpg" class="img-rounded img-chart " style="width:170px;height:170px;"></a></div>				
-			<div><a href="/Mimo/playlist" class="txt ">Electronic Music</a></div>	
+			<div><a href="/Mimo/playlist" class="txt " style="color:black">Electronic Music</a></div>	
 		</div>
 	</div>
 	
@@ -116,7 +116,7 @@
 		<div>
 			<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/european.png" class="img-rounded img-chart " style="width:170px;height:170px"></a></div>					
-			<div><a href="/Mimo/playlist" class="txt">European Music</a></div>	
+			<div><a href="/Mimo/playlist" class="txt" style="color:black">European Music</a></div>	
 		</div>
 	</div>
 	
@@ -124,21 +124,21 @@
 		<div>
 			<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/hip.jpg" class="img-rounded img-chart"  style="width:170px;height:170px "></a></div>				
-			<div><a href="/Mimo/playlist" class="txt">Hip Hop / Rap</a></div>
+			<div><a href="/Mimo/playlist" class="txt" style="color:black">Hip Hop / Rap</a></div>
 		</div>
 	</div>
 	<div class="col-md-3 " >
 		<div>
 			<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/indie.png" class="img-rounded img-chart"  style="width:170px;height:170px "></a></div>				
-			<div><a href="/Mimo/playlist" class="txt">Indie Pop</a></div>
+			<div><a href="/Mimo/playlist" class="txt" style="color:black">Indie Pop</a></div>
 		</div>
 	</div>
 	<div class="col-md-3 " >
 		<div>
 		<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/inspire.jpg" class="img-rounded img-chart " style="width:170px;height:170px;"></a></div>				
-			<div><a href="/Mimo/playlist" class="txt ">Inspirational Music</a></div>	
+			<div><a href="/Mimo/playlist" class="txt " style="color:black">Inspirational Music</a></div>	
 		</div>
 	</div>
 	
@@ -146,7 +146,7 @@
 		<div>
 			<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/new.jpg" class="img-rounded img-chart " style="width:170px;height:170px"></a></div>					
-			<div><a href="/Mimo/playlist" class="txt">New Age Music</a></div>	
+			<div><a href="/Mimo/playlist" class="txt" style="color:black">New Age Music</a></div>	
 		</div>
 	</div>
 	
@@ -154,14 +154,14 @@
 		<div>
 			<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/opera.jpg" class="img-rounded img-chart"  style="width:170px;height:170px "></a></div>				
-			<div><a href="/Mimo/playlist" class="txt">Opera</a></div>
+			<div><a href="/Mimo/playlist" class="txt" style="color:black">Opera</a></div>
 		</div>
 	</div>
 	<div class="col-md-3 " >
 		<div>
 		<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/pop.jpg" class="img-rounded img-chart " style="width:170px;height:170px;"></a></div>				
-			<div><a href="/Mimo/playlist" class="txt ">Pop Music</a></div>	
+			<div><a href="/Mimo/playlist" class="txt" style="color:black">Pop Music</a></div>	
 		</div>
 	</div>
 	
@@ -169,7 +169,7 @@
 		<div>
 			<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/rnb.jpg" class="img-rounded img-chart " style="width:170px;height:170px"></a></div>					
-			<div><a href="/Mimo/playlist" class="txt">R&B / Soul</a></div>	
+			<div><a href="/Mimo/playlist" class="txt" style="color:black">R&B / Soul</a></div>	
 		</div>
 	</div>
 	
@@ -177,14 +177,14 @@
 		<div>
 			<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/reggae.jpg" class="img-rounded img-chart"  style="width:170px;height:170px "></a></div>				
-			<div><a href="/Mimo/playlist" class="txt">Reggae</a></div>
+			<div><a href="/Mimo/playlist" class="txt" style="color:black">Reggae</a></div>
 		</div>
 	</div>
 	<div class="col-md-3 " >
 		<div>
 		<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/rock.jpg" class="img-rounded img-chart " style="width:170px;height:170px;"></a></div>				
-			<div><a href="/Mimo/playlist" class="txt ">Rock Music</a></div>	
+			<div><a href="/Mimo/playlist" class="txt" style="color:black">Rock Music</a></div>	
 		</div>
 	</div>
 	
@@ -192,7 +192,7 @@
 		<div>
 			<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/singer.jpg" class="img-rounded img-chart " style="width:170px;height:170px"></a></div>					
-			<div><a href="/Mimo/playlist" class="txt">Singer / Songwriter</a></div>	
+			<div><a href="/Mimo/playlist" class="txt" style="color:black">Singer / Songwriter</a></div>	
 		</div>
 	</div>
 	
@@ -200,7 +200,7 @@
 		<div>
 			<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/world.jpg" class="img-rounded img-chart"  style="width:170px;height:170px "></a></div>				
-			<div><a href="/Mimo/playlist" class="txt">World Music / Beats</a></div>
+			<div><a href="/Mimo/playlist" class="txt" style="color:black">World Music / Beats</a></div>
 		</div>
 	</div>
 </nav>
--- application/views/mimo_v/genre.php
+++ application/views/mimo_v/genre.php
@@ -1,6 +1,6 @@
 <body style="background: linear-gradient(to bottom right, #bdc3c7 1%, #2c3e50 111%);
 			background-attachment: fixed">
-	<!-- Top Nav Bar -->
+	<!-- Top Nav Bar --> 
 
 	<!--This is below Top Navigation Section -->
 <div class="col-md-10" style="padding-top: 80px; margin-left: 100px;  ">
@@ -16,15 +16,14 @@
 			</div>
 <div class="container " style="padding-top:50px;">
 <div class="margin">
-	<h3 class="txt" style="color:white;">Genres</h3>
+	<h2 class="txt" style="color:white;">Genres</h2>
 </div>
-
-<div class="row text-center" style="padding-top:50px;">
+<div class="row text-center" style="padding-top:20px;">
 	<div class="col-md-2 " >
 		<div >
 			<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist" ><img src="http://localhost/mimo/assets/img/alt.png" class="img-rounded img-chart "></a></div>			
-			<div><a href="/Mimo/playlist" class="txt ">Alternative Music</a></div>	
+			<div><a href="/Mimo/playlist" class="txt" style="color: black;">Alternative Music</a></div>	
 		</div>
 	</div>
 	
@@ -33,7 +32,7 @@
 		<div>
 		<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/blues.jpg" class="img-rounded img-chart " style="width:170px;height:170px;"></a></div>				
-			<div><a href="/Mimo/playlist" class="txt ">Blues</a></div>	
+			<div><a href="/Mimo/playlist" class="txt " style="color: black;">Blues</a></div>	
 		</div>
 	</div>
 	
@@ -41,7 +40,7 @@
 		<div>
 			<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/classical.png" class="img-rounded img-chart " style="width:170px;height:170px"></a></div>					
-			<div><a href="/Mimo/playlist" class="txt">Classical Music</a></div>	
+			<div><a href="/Mimo/playlist" class="txt" style="color: black;">Classical Music</a></div>	
 		</div>
 	</div>
 	
@@ -49,14 +48,14 @@
 		<div>
 			<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/country.png" class="img-rounded img-chart"  style="width:170px;height:170px "></a></div>				
-			<div><a href="/Mimo/playlist" class="txt">Country Music</a></div>
+			<div><a href="/Mimo/playlist" class="txt" style="color: black;">Country Music</a></div>
 		</div>
 	</div>
 	<div class="col-md-2 " >
 		<div>
 		<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/dance.jpg" class="img-rounded img-chart " style="width:170px;height:170px;"></a></div>				
-			<div><a href="/Mimo/playlist" class="txt ">Dance Music</a></div>	
+			<div><a href="/Mimo/playlist" class="txt " style="color: black;">Dance Music</a></div>	
 		</div>
 	</div>
 	
@@ -64,7 +63,7 @@
 		<div>
 			<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/easy.png" class="img-rounded img-chart " style="width:170px;height:170px"></a></div>					
-			<div><a href="/Mimo/playlist" class="txt">Easy Listening</a></div>	
+			<div><a href="/Mimo/playlist" class="txt" style="color: black;">Easy Listening</a></div>	
 		</div>
 	</div>
 	
@@ -73,7 +72,7 @@
 		<div>
 		<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/electronic.jpg" class="img-rounded img-chart " style="width:170px;height:170px;"></a></div>				
-			<div><a href="/Mimo/playlist" class="txt ">Electronic Music</a></div>	
+			<div><a href="/Mimo/playlist" class="txt " style="color: black;">Electronic Music</a></div>	
 		</div>
 	</div>
 	
@@ -81,7 +80,7 @@
 		<div>
 			<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/european.png" class="img-rounded img-chart " style="width:170px;height:170px"></a></div>					
-			<div><a href="/Mimo/playlist" class="txt">European Music</a></div>	
+			<div><a href="/Mimo/playlist" class="txt" style="color: black;">European Music</a></div>	
 		</div>
 	</div>
 	
@@ -89,21 +88,21 @@
 		<div>
 			<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/hip.jpg" class="img-rounded img-chart"  style="width:170px;height:170px "></a></div>				
-			<div><a href="/Mimo/playlist" class="txt">Hip Hop / Rap</a></div>
+			<div><a href="/Mimo/playlist" class="txt" style="color: black;">Hip Hop / Rap</a></div>
 		</div>
 	</div>
 	<div class="col-md-2 " >
 		<div>
 			<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/indie.png" class="img-rounded img-chart"  style="width:170px;height:170px "></a></div>				
-			<div><a href="/Mimo/playlist" class="txt">Indie Pop</a></div>
+			<div><a href="/Mimo/playlist" class="txt" style="color: black;">Indie Pop</a></div>
 		</div>
 	</div>
 	<div class="col-md-2 " >
 		<div>
 		<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/inspire.jpg" class="img-rounded img-chart " style="width:170px;height:170px;"></a></div>				
-			<div><a href="/Mimo/playlist" class="txt ">Inspirational Music</a></div>	
+			<div><a href="/Mimo/playlist" class="txt " style="color: black;">Inspirational Music</a></div>	
 		</div>
 	</div>
 	
@@ -111,7 +110,7 @@
 		<div>
 			<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/new.jpg" class="img-rounded img-chart " style="width:170px;height:170px"></a></div>					
-			<div><a href="/Mimo/playlist" class="txt">New Age Music</a></div>	
+			<div><a href="/Mimo/playlist" class="txt" style="color: black;">New Age Music</a></div>	
 		</div>
 	</div>
 	
@@ -119,14 +118,14 @@
 		<div>
 			<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/opera.jpg" class="img-rounded img-chart"  style="width:170px;height:170px "></a></div>				
-			<div><a href="/Mimo/playlist" class="txt">Opera</a></div>
+			<div><a href="/Mimo/playlist" class="txt" style="color: black;">Opera</a></div>
 		</div>
 	</div>
 	<div class="col-md-2 " >
 		<div>
 		<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/pop.jpg" class="img-rounded img-chart " style="width:170px;height:170px;"></a></div>				
-			<div><a href="/Mimo/playlist" class="txt ">Pop Music</a></div>	
+			<div><a href="/Mimo/playlist" class="txt " style="color: black;">Pop Music</a></div>	
 		</div>
 	</div>
 	
@@ -134,7 +133,7 @@
 		<div>
 			<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/rnb.jpg" class="img-rounded img-chart " style="width:170px;height:170px"></a></div>					
-			<div><a href="/Mimo/playlist" class="txt">R&B / Soul</a></div>	
+			<div><a href="/Mimo/playlist" class="txt" style="color: black;">R&B / Soul</a></div>	
 		</div>
 	</div>
 	
@@ -142,14 +141,14 @@
 		<div>
 			<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/reggae.jpg" class="img-rounded img-chart"  style="width:170px;height:170px "></a></div>				
-			<div><a href="/Mimo/playlist" class="txt">Reggae</a></div>
+			<div><a href="/Mimo/playlist" class="txt" style="color: black;">Reggae</a></div>
 		</div>
 	</div>
 	<div class="col-md-2 " >
 		<div>
 		<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/rock.jpg" class="img-rounded img-chart " style="width:170px;height:170px;"></a></div>				
-			<div><a href="/Mimo/playlist" class="txt ">Rock Music</a></div>	
+			<div><a href="/Mimo/playlist" class="txt " style="color: black;">Rock Music</a></div>	
 		</div>
 	</div>
 	
@@ -157,7 +156,7 @@
 		<div>
 			<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/singer.jpg" class="img-rounded img-chart " style="width:170px;height:170px"></a></div>					
-			<div><a href="/Mimo/playlist" class="txt">Singer / Songwriter</a></div>	
+			<div><a href="/Mimo/playlist" class="txt" style="color: black;">Singer / Songwriter</a></div>	
 		</div>
 	</div>
 	
@@ -165,7 +164,7 @@
 		<div>
 			<!--both a href link papunta sa playlist-->
 			<div><a href="/Mimo/playlist"><img src="http://localhost/mimo/assets/img/world.jpg" class="img-rounded img-chart"  style="width:170px;height:170px "></a></div>				
-			<div><a href="/Mimo/playlist" class="txt">World Music / Beats</a></div>
+			<div><a href="/Mimo/playlist" class="txt" style="color: black;">World Music / Beats</a></div>
 		</div>
 	</div>
 	<?php $this->load->view('include/releasebuttons');?>
