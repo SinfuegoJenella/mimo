e48dac8b66dc5bfacc0df45012c7b65edf195124
sammillos <millossamantha@gmail.com>
2017-10-16 22:39:31 +0800

    addtocollection

--- application/views/mimo_v/home.php
+++ application/views/mimo_v/home.php
@@ -1,57 +1,70 @@
-<body style="background-color: #e1e1e1">
+<body style="background-color: #e1e1e1; background-color: ">
 	<!-- Top Nav Bar -->
 		<br>
 		
-<div class="container">
-  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-    </ol>
+<div class="container-fluid">
+	<div class="row">
+	
+		<div class="col-md-8" style="background-color: #e1e1e1">
+			
+		</div>
+		
+		
+		<div class="col-md-4">
+			<div id="myCarousel" class="carousel slide" data-ride="carousel" style="float:right">
+				<!-- Indicators -->
+				<ol class="carousel-indicators">
+					<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+					<li data-target="#myCarousel" data-slide-to="1"></li>
+					<li data-target="#myCarousel" data-slide-to="2"></li>
+				</ol>
 
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner">
+				<!-- Wrapper for slides -->
+			<div class="carousel-inner">
+				<div class="item active">
+					<img src="http://localhost/mimo/assets/img/ilovemusic1.jpg" alt="Los Angeles" style="width:100%;height: 250px">
+						<div class="carousel-caption">
+							<h4>Share your Talent!</h3>
+							<p>Let others know your amazing musical talent through audios and video.</p>
+						</div>
+				</div>
 
-      <div class="item active">
-        <img src="http://localhost/mimo/assets/img/ny.jpg" alt="Los Angeles" style="width:100%;height: 350px">
-        <div class="carousel-caption">
-          <h3>Los Angeles</h3>
-          <p>LA is always so much fun!</p>
-        </div>
-      </div>
-
-      <div class="item">
-        <img src="http://localhost/mimo/assets/img/mimo1.jpg" alt="Chicago" style="width:100%;">
-        <div class="carousel-caption">
-          <h3>Chicago</h3>
-          <p>Thank you, Chicago!</p>
-        </div>
-      </div>
+				<div class="item">
+					<img src="http://localhost/mimo/assets/img/mp3.jpg" alt="Chicago" style="width:100%; height: 180px">
+					<div class="carousel-caption">
+						<h4>Sore High!</h3>
+						<p>Gain followers and be on the Top 10 Most Followed Artist.</p>
+					</div>
+				</div>
     
-      <div class="item">
-        <img src="http://localhost/mimo/assets/img/mimo1.jpg" alt="New York" style="width:100%;">
-        <div class="carousel-caption">
-          <h3>New York</h3>
-          <p>We love the Big Apple!</p>
-        </div>
-      </div>
-  
-    </div>
+				<div class="item">
+					<img src="http://localhost/mimo/assets/img/singer.jpg" alt="New York" style="width:100%; height: 180px">
+					<div class="carousel-caption">
+						<h4>Discover or be discovered!</h3>
+						<p>Show the world what youu got and let others discover you amazing talent.</p>
+					</div>
+				</div>
+			</div>
 
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right"></span>
-      <span class="sr-only">Next</span>
-    </a>
-  </div>
+			<!-- Left and right controls -->
+			<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+				<span class="glyphicon glyphicon-chevron-left"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="right carousel-control" href="#myCarousel" data-slide="next">
+				<span class="glyphicon glyphicon-chevron-right"></span>
+				<span class="sr-only">Next</span>
+			</a>
+			</div>
+			
+			
+			<div class="row">
+				<h4>Top 10 Most Followed Artists</h4>
+			</div>
+		</div>
+		
+	</div>
 </div>
-
 
 <div class="container" style="margin-top: 10px" >
 
---
+++ application/views/templates/colmodal.php
@@ -0,0 +1,49 @@
+<div class="modal fade" id="collectModal" tabindex="-1" role="dialog" 
+		aria-hidden="true" style="margin-top: 100px; margin: auto;">
+		<div class="modal-dialog">
+			<div class="modal-content">
+				<!-- Modal Body -->
+				<div class="modal-body" style="background-color: #f9f9f9">
+				<button type="button" class="close" data-dismiss="modal">
+                       <span aria-hidden="true">&times;</span>
+                       <span class="sr-only">Close</span>
+					</button>
+					
+				<h4 class="modal-title text-center" id="myModalLabel">
+					Add this audio to:
+				</h4>
+				<hr />
+			<form class="form-inline" style="margin: 10px auto">
+				<div class="row">
+					<div class="col-md-6">
+						<label for="sel1"><span class="material-icons">library_music</span>Select:</label>
+						<select class="form-control" id="sel1" style="width: 100%">
+							<option></option>
+							<option>ooption1df</option>
+							<option>3hbjnmbjnvgb</option>
+							<option>4ygvbtvgbhn</option>
+						</select>
+					</div>
+					
+					<div class="col-md-6">
+						<label for="sel1"><span class="material-icons">add_to_photos</span> or Add New:</label>
+						<input style="width: 100%" type="text" class="form-control" id="text" placeholder="Add New Collection" />
+					</div>
+				
+				</div>
+				
+				<div class="row">
+					<div class="col-md-12">
+						<button type="button" class="btn btn-warning pull-right" id="collectModal">
+							OK!
+						</button>
+					</div>
+				</div>
+			</form>
+
+				
+					</div>
+				</div>
+			</div>
+		</div>
+		<!--End of Add Thought Modal-->
