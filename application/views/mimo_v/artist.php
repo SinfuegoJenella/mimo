c9adc884d5c7554b311dcd592430dfc5c737165d
sammillos <millossamantha@gmail.com>
2017-10-16 15:18:10 +0800

    Update artist.php

--- application/views/mimo_v/artist.php
+++ application/views/mimo_v/artist.php
@@ -37,43 +37,6 @@
 			</div>
 		</div>
 		
-<<<<<<< HEAD
-<div class="col-md-6" style="padding-top: 40px; ">
-<div class="container"><div class="col-lg-12"><p><a href="#"><h3>New MimO Artists</h3></a></p></div>
-
-<br/>
-<a href="#"><div class="container"><div class="col-lg-12"><p style="color: white;"><i class="material-icons ">music_note</i> nightingale07 &emsp; <button type="submit" form="form1" value="Submit" class="btn pill" id="btn">Follow</button></p></div></div></a>
-<a href="#"><div class="container"><div class="col-lg-12"><p style="color: white;"><i class="material-icons">music_note</i> IzzayBenedicto &emsp;<button type="submit" form="form1" value="Submit" class="btn pill" id="btn" >Follow</button></p></div></div></a>
-<a href="#"><div class="container"><div class="col-lg-12"><p style="color: white;"><i class="material-icons">music_note</i> CarlyyAbends &emsp;<button type="submit" form="form1" value="Submit" class="btn pill" id="btn">Follow</button></p></div></div></a>
-<a href="#"><div class="container"><div class="col-lg-12"><p style="color: white;"><i class="material-icons">music_note</i> GeloGeloGelo &emsp; <button type="submit" form="form1" value="Submit" class="btn pill" id="btn">Follow</button></p></div></div></a>
-<a href="#"><div class="container"><div class="col-lg-12"><p style="color: white;"><i class="material-icons">music_note</i> SheiShei &emsp;<button type="submit" form="form1" value="Submit" class="btn pill" id="btn">Follow</button> </p></div></div></a>
-<a href="#"><div class="container"><div class="col-lg-12"><p style="color: white;"><i class="material-icons">music_note</i> JenJen &emsp;<button type="submit" form="form1" value="Submit" class="btn pill" id="btn">Follow</button> </p></div></div></a>
-<a href="#"><div class="container"><div class="col-lg-12"><p style="color: white;"><i class="material-icons">music_note</i> Geisheeeerrr &emsp; <button type="submit" form="form1" value="Submit" class="btn pill" id="btn">Follow</button></p></div></div></a>
-<a href="#"><div class="container"><div class="col-lg-12"><p style="color: white;"><i class="material-icons">music_note</i> Audeng &emsp; <button type="submit" form="form1" value="Submit" class="btn pill" id="btn">Follow</button></p></div></div></a>
-
-</div>
-</div>
-<div class="col-md-5" style="padding-top: 10px; ">
-			<h3 style="color: white;" class="user"></i><b style="color: #ff9926; "> Top 20 Mimo Artists</b></h3>
-
-			<div class="tbl">
-		<table class="table">
-			<thead>
-				<tr class="p text-center ">
-					<th> </th>
-					<th style="color: white;"> </th>
-					<th style="color: white;"></th>
-					<th class="hidden-xs" style="color: white;"> </th>
-				</tr>
-			</thead>
-
-			<div class="row" style="background-color: rgba(255,255,255,0.1); padding: 20px; margin-top: 10px;">
-				<a href="<?php echo base_url('mimo/artistplaylist');?>" ><div class="container"><div class="col-lg-12"><p style="color: white;"><img src="http://localhost/mimo/assets/img/noimage.jpg" class="img-circle" style="width: 40px; height: 40px;" alt="noimage"/> &emsp; MoiraDelaTorre <a style="color: gray;"> - 173,263 followers </a></p></div></div></a>
-				<a href="<?php echo base_url('mimo/artistplaylist');?>"><div class="container"><div class="col-lg-12"><p style="color: white;"><img src="http://localhost/mimo/assets/img/noimage.jpg" class="img-circle" style="width: 40px; height: 40px;" alt="noimage"/> &emsp; InigoooP <a style="color: gray;"> - 173,263 followers </a></p></div></div></a>
-				<a href="<?php echo base_url('mimo/artistplaylist');?>"><div class="container"><div class="col-lg-12"><p style="color: white;"><img src="http://localhost/mimo/assets/img/noimage.jpg" class="img-circle" style="width: 40px; height: 40px;" alt="noimage"/> &emsp; MarisRacal <a style="color: gray;"> - 173,263 followers </a></p></div></div></a>
-				
-
-=======
 		<div style="background-color: #e1e1e1; height:150px; margin-top: 5%;" >
 			<div style="float: left; padding-top:6%; padding-left:6%;">
 				<div><a href="<?php echo base_url('mimo/artistplaylist');?>" ><img src="http://localhost/mimo/assets/img/IU.jpg" class="img-circle img"></a></div>
