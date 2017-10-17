eb86903ead0b19d5005229c28e40655ec8ccc825
Shei <s.cmanawit@gmail.com>
2017-10-18 00:53:18 +0800

    collections+updated sql

--- application/models/upload.php
+++ application/models/upload.php
@@ -7,4 +7,23 @@ class upload extends CI_Model {
 		$this->db->insert($table, $data);
 		return TRUE;	
 	}
+	public function c(){
+		$lastid = $this->db->insert_id();
+		return $lastid;
+	}
+	public function select($table,$condition=null,$selector=null){
+
+		if($selector==null) $selector = '*';
+		$this->db->select($selector);
+		$this->db->from($table);
+		if( isset($condition) ) $this->db->where($condition);
+		$query=$this->db->get();
+
+		return $query->result_array();		
+	}
+	public function update($table,$data,$condition){
+		$this->db->where($condition);
+		$this->db->update($table, $data);
+		return TRUE;	
+	}
 }
