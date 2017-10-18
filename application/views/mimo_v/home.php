0e16e31fe28b03287df136401be5953482a67d79
sammillos <millossamantha@gmail.com>
2017-10-18 21:10:23 +0800

    collection view

--- application/views/mimo_v/home.php
+++ application/views/mimo_v/home.php
@@ -1,7 +1,116 @@
-<body style="background-color: #e1e1e1; background-color: ">
+<body style="background-color: #e1e1e1; background-image: url('http://localhost/mimo/assets/img/happy.jpg'); background-size:cover; background-attachment:fixed; background-color: rgba(255,255,255,0.9); margin:0 0; background-repeat: no-repeat">
 	<!-- Top Nav Bar -->
+	<?php $this->load->view('include/log-outnav');?>
 		<br>
+
+<div class="container" style="position:absolute; padding: 20px; padding-top: 0; width:100%" >
+<div class="row">
+
+</div>
+
+
+<div class="row belowtn" style="margin-right: 10px; ">
+		<div class="col-md-3">
+			
+				<div class="row" style="margin-left: 15px; margin-right:15px">
+					<div class="col-md-12" style="background-color: #808080; color: white; padding: 10px; margin-left: 10px; margin-right:15px">
+						<h4 class="text-center" style="color: white"><?php $this->load->view('include/mimologo')?> Top 10 Most Followed Artist</h4>
+					</div>
+				
+					<div class="row ">
+					<!--TOP LIST-->
+					<div class="col-md-12" style="margin-left: 10px">
+					<?php $this->load->view('templates/toplist');?>
+					<?php $this->load->view('templates/toplist');?>
+					<?php $this->load->view('templates/toplist');?>
+					<?php $this->load->view('templates/toplist');?>
+					<?php $this->load->view('templates/toplist');?>
+					<?php $this->load->view('templates/toplist');?>
+					<?php $this->load->view('templates/toplist');?>
+					<?php $this->load->view('templates/toplist');?>
+					<?php $this->load->view('templates/toplist');?>
+					<?php $this->load->view('templates/toplist');?>
+					</div>
+					<!--END OF TOP LIST-->
+					
+				</div>
+		</div>
+		</div>
+		<div class="col-md-9">
+	
+		<ul class="nav nav-tabs nav-justified searchtab">
+			<!-- COLLECTIONS, ALBUMS, THOUGHTS, and FAVORITES TABS-->
+			<li class="active"><a data-toggle="tab" href="#sartist">&nbsp;Discover Artist</a></li>
+			<li><a data-toggle="tab" href="#sthought">&nbsp;Random Thoughts</a></li>
+			<li><a data-toggle="tab" href="#saudios">&nbsp;Top Audios</a></li>
+			<li><a data-toggle="tab" href="#svids">&nbsp;Top Videos</a></li>
+		</ul>
+
+			<div class="tab-content" style="background-color: rgba(255,255,255,0.9); height: 600px">
+				<?php $this->load->view('templates/commentModal');?>
+					<!-- ARTIST -->
+						<div id="sartist" class="tab-pane fade in active">
+								<div class="row">
+									<div class="col-md-1"></div>
+									<div class="col-md-10 artist">
+										<h2>New Artist</h2>
+									</div>
+									<div class="col-md-1"></div>
+								</div>
+							</div>
+   
+   
+						<!-- AUDIOS-->
+							<div id="sthought" class="tab-pane fade" >
+								<div class="row">
+									<div class="col-md-1"></div>
+									<div class="col-md-10 thoughts">
+									</div>
+									<div class="col-md-1"></div>
+								</div>
+							</div>
+							
+							
+							<!-- THOUGHTS-->
+							<div id="saudios" class="tab-pane fade">
+								<div class="row">
+									<div class="col-md-1"></div>
+									<div class="col-md-10 audios">
+									</div>
+									<div class="col-md-1"></div>
+								</div>
+							</div>	
+							
+							<!-- FAVORITES-->
+							<div id="svids" class="tab-pane fade">
+								<div class="row">
+									<div class="col-md-1"></div>
+									<div class="col-md-10 videos">
+									</div>
+									<div class="col-md-1"></div>
+								</div>
+							</div>
+					</div>
+					
+			</div>	
+	</div>
 		
+			</div>
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+<!--	
 <div class="container-fluid">
 	<div class="row">
 	
@@ -12,14 +121,14 @@
 		
 		<div class="col-md-4">
 			<div id="myCarousel" class="carousel slide" data-ride="carousel" style="float:right">
-				<!-- Indicators -->
+				<!-- Indicators 
 				<ol class="carousel-indicators">
 					<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
 					<li data-target="#myCarousel" data-slide-to="1"></li>
 					<li data-target="#myCarousel" data-slide-to="2"></li>
 				</ol>
 
-				<!-- Wrapper for slides -->
+				<!-- Wrapper for slides 
 			<div class="carousel-inner">
 				<div class="item active">
 					<img src="http://localhost/mimo/assets/img/ilovemusic1.jpg" alt="Los Angeles" style="width:100%;height: 250px">
@@ -46,7 +155,7 @@
 				</div>
 			</div>
 
-			<!-- Left and right controls -->
+			<!-- Left and right controls
 			<a class="left carousel-control" href="#myCarousel" data-slide="prev">
 				<span class="glyphicon glyphicon-chevron-left"></span>
 				<span class="sr-only">Previous</span>
@@ -64,11 +173,11 @@
 		</div>
 		
 	</div>
-</div>
+</div>-->
+<!--
+<div class="container belowtn" style="margin-top: 10px" >
 
-<div class="container" style="margin-top: 10px" >
-
-	<div class="row" class="" style="background-color:white">
+	<div class="row" class="" style="background-color:rgba(255,255,255,0.9)">
 		<div class="vtab">
 			<button class="tablinks" onclick="openTab(event, 'Audios')" id="defaultOpen">Audios</button>
 			<button class="tablinks" onclick="openTab(event, 'Artists')">Artist</button>
@@ -104,12 +213,7 @@
 		</div>
 	</div>
 </div>
-
-<div class="container-fluid" style="margin-top: 0px" >
-	<div class="row">
-		
-	</div>
-</div>
+-->
 </body>
 <script>
 function openTab(evt, tabName) {
--- assets/js/audplay.js
+++ assets/js/audplay.js
@@ -98,3 +98,75 @@
   // }
   // return _mprefix + _min + ":" + _sprefix + _time;
 // }
+
+
+
+
+
+// function setCookie(c_name,value,exdays)
+// {
+    // var exdate=new Date();
+    // exdate.setDate(exdate.getDate() + exdays);
+    // var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());
+    // document.cookie=c_name + "=" + c_value;
+// }
+
+// function getCookie(c_name)
+// {
+    // var i,x,y,ARRcookies=document.cookie.split(";");
+    // for (i=0;i<ARRcookies.length;i++)
+    // {
+      // x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+      // y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+      // x=x.replace(/^\s+|\s+$/g,"");
+      // if (x==c_name)
+        // {
+        // return unescape(y);
+        // }
+      // }
+// }
+
+// var song = document.getElementsByTagName('audio')[0];
+// var played = false;
+// var tillPlayed = getCookie('timePlayed');
+
+
+// function update()
+// {
+    // if(!played){
+        // if(tillPlayed){
+        // song.currentTime = tillPlayed;
+        // song.play();
+        // played = true;
+        // }
+        // else {
+                // song.play();
+                // played = true;
+        // }
+    // }
+
+    // else {
+
+    // setCookie('timePlayed', song.currentTime);
+	
+    // }
+// }
+// setInterval(update,1000);
+
+// </script>
+// <script type="text/javascript">
+    // if (window.location.hash && window.location.hash == '#_=_') {
+        // if (window.history && history.pushState) {
+            // window.history.pushState("", document.title, window.location.pathname);
+        // } else {
+            Prevent scrolling by storing the page's current scroll offset
+            // var scroll = {
+                // top: document.body.scrollTop,
+                // left: document.body.scrollLeft
+            // };
+            // window.location.hash = '';
+            Restore the scroll offset, should be flicker free
+            // document.body.scrollTop = scroll.top;
+            // document.body.scrollLeft = scroll.left;
+        // }
+    // }
