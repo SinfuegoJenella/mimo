30cf61898eebdf1e5fc82b6471dda91f0ff9021a
Izzay <izzaybene@gmail.com>
2017-10-21 09:59:32 +0800

    browse po

--- application/views/templates/s_video.php
+++ application/views/templates/s_video.php
@@ -41,3 +41,132 @@
 				</div>	
 	</div>
 </div>
+<div class="searchbox">
+	<div class="media">
+		<div class="media-left">
+			<a href="#" >
+				<div class="media-object srchThoughtPic" style="background-image:url('http://localhost/mimo/assets/img/bts.jpg');"></div>
+			</a>
+		</div>
+
+		<div class="media-body">
+			<h4 class="media-heading"><a href="">BTS - Serendipity (Comeback Teaser)</a>
+				<br /><small>by: <a href="">BTS</a></small>
+			</h4>
+			<p id="audDesc">This is for the video description! ^^</p>
+			<div class="row" >
+				<div class="col-md-12">
+					<video src="http://localhost/mimo/assets/videos/Serendipity.mp4" style="width:100%; height: 200px" controls controlsList="nodownload">
+					</video>
+				</div>
+			</div>
+		</div>	
+		
+			<div id="likesectionaud">
+				<div class="btn-grp btn-group-justified">
+						<a href="#" id="likeBtn" type="button" class="btn like">
+						<!-- Like -->
+						<span class="fa fa-heart-o"></span> 
+						<small><small>(127,343)</small></small></a>
+						
+						
+						<!--Comment -->
+						<a id="commentBtn" class="btn comment" data-toggle="modal" data-target="#commentModal">
+						<span class="fa fa-commenting-o"></span>
+						<small><small>(873)</small></small></a>
+	
+						<a class="btn view disabled">
+						<span style="font-size: 12px;" class="glyphicon glyphicon-play"></span>
+						<small> 123,234</small>
+						</a>
+				
+				</div>
+				</div>	
+	</div>
+</div>
+<div class="searchbox">
+	<div class="media">
+		<div class="media-left">
+			<a href="#" >
+				<div class="media-object srchThoughtPic" style="background-image:url('http://localhost/mimo/assets/img/bts.jpg');"></div>
+			</a>
+		</div>
+
+		<div class="media-body">
+			<h4 class="media-heading"><a href="">BTS - Serendipity (Comeback Teaser)</a>
+				<br /><small>by: <a href="">BTS</a></small>
+			</h4>
+			<p id="audDesc">This is for the video description! ^^</p>
+			<div class="row" >
+				<div class="col-md-12">
+					<video src="http://localhost/mimo/assets/videos/Serendipity.mp4" style="width:100%; height: 200px" controls controlsList="nodownload">
+					</video>
+				</div>
+			</div>
+		</div>	
+		
+			<div id="likesectionaud">
+				<div class="btn-grp btn-group-justified">
+						<a href="#" id="likeBtn" type="button" class="btn like">
+						<!-- Like -->
+						<span class="fa fa-heart-o"></span> 
+						<small><small>(127,343)</small></small></a>
+						
+						
+						<!--Comment -->
+						<a id="commentBtn" class="btn comment" data-toggle="modal" data-target="#commentModal">
+						<span class="fa fa-commenting-o"></span>
+						<small><small>(873)</small></small></a>
+	
+						<a class="btn view disabled">
+						<span style="font-size: 12px;" class="glyphicon glyphicon-play"></span>
+						<small> 123,234</small>
+						</a>
+				
+				</div>
+				</div>	
+	</div>
+</div>
+<div class="searchbox">
+	<div class="media">
+		<div class="media-left">
+			<a href="#" >
+				<div class="media-object srchThoughtPic" style="background-image:url('http://localhost/mimo/assets/img/bts.jpg');"></div>
+			</a>
+		</div>
+
+		<div class="media-body">
+			<h4 class="media-heading"><a href="">BTS - Serendipity (Comeback Teaser)</a>
+				<br /><small>by: <a href="">BTS</a></small>
+			</h4>
+			<p id="audDesc">This is for the video description! ^^</p>
+			<div class="row" >
+				<div class="col-md-12">
+					<video src="http://localhost/mimo/assets/videos/Serendipity.mp4" style="width:100%; height: 200px" controls controlsList="nodownload">
+					</video>
+				</div>
+			</div>
+		</div>	
+		
+			<div id="likesectionaud">
+				<div class="btn-grp btn-group-justified">
+						<a href="#" id="likeBtn" type="button" class="btn like">
+						<!-- Like -->
+						<span class="fa fa-heart-o"></span> 
+						<small><small>(127,343)</small></small></a>
+						
+						
+						<!--Comment -->
+						<a id="commentBtn" class="btn comment" data-toggle="modal" data-target="#commentModal">
+						<span class="fa fa-commenting-o"></span>
+						<small><small>(873)</small></small></a>
+	
+						<a class="btn view disabled">
+						<span style="font-size: 12px;" class="glyphicon glyphicon-play"></span>
+						<small> 123,234</small>
+						</a>
+				
+				</div>
+				</div>	
+	</div>
+</div>
